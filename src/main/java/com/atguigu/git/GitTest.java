a8bd65917dade287c9158126d8b3fecfa08cdb13
liangliang <820670961@qq.com>
2021-05-13 14:54:48 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -1,6 +1,6 @@
 package com.atguigu.git;
 
-public class GitTest {
+public class GitTest{
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2!");
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git4!");
         System.out.println("master test!");
         System.out.println("hot-fix test!");
+        System.out.println("push test!");
     }
 }
